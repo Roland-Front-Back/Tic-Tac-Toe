aed4c2ec82b26f1e764f6b18d221d6430320763f
Roland Amoguis <185738272+Roland-Front-Back@users.noreply.github.com>
2025-08-01 14:14:41 +0800

    create a third function factory GameController > handles the flow and state of the game's logic creates player marks and winning pattern and convert 2d arr. into 1d to more precise value retrieval

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,91 @@ function Cell() {
     getValue,
   };
 }
+
+// GameController function factory
+// Handles flow and state of the game's logic
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = GameBoard();
+
+  const players = [
+    { name: playerOneName, mark: "X" },
+    { name: playerTwoName, mark: "O" },
+  ];
+
+  let activePlayer = players[0];
+  let isWinner = false;
+  let isTie = false;
+
+  const getBoard = board.getBoard;
+  const getIsWinner = () => isWinner;
+  const getIsTie = () => isTie;
+  const getActivePlayer = () => activePlayer; // retrieve active player
+
+  // Switches turn to other player
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  // Flatten the board to 1d arr. of values
+  const getFlatBoard = () => {
+    board
+      .getBoard()
+      .flat()
+      .map((cell) => cell.getValue());
+  };
+
+  // Checks if current player has a winning patter
+  const checkWinner = (playerMark) => {
+    const winPattern = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    const boardFlat = getBoard();
+
+    // returns if player mark === to winpattern
+    return winPattern.some((pattern) =>
+      pattern.every((index) => boardFlat[index] === playerMark)
+    );
+  };
+
+  // Checks if all cells are filled => draw
+  const checkTie = () => {
+    return getFlatBoard().every((mark) => mark !== "");
+  };
+
+  // Process a player move
+  const playRound = (row, column) => {
+    if (isWinner || isTie) return;
+
+    // Only play if cell is empty
+    if (board.getBoard()[row][column].getValue() !== "") return;
+
+    board.placeMark(row, column, activePlayer.mark);
+
+    if (checkWinner(activePlayer.mark)) {
+      isWinner = true;
+    } else if (checkTie()) {
+      isTie = true;
+    } else {
+      switchPlayerTurn(); // no win or tie => continue game
+    }
+  };
+
+  return {
+    playRound,
+    getBoard,
+    getActivePlayer,
+    getIsWinner,
+    getIsTie,
+  };
+}
